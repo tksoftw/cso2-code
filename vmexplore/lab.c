92371a34fd0e1a4beeebeeed3e63dfd79916b6c2
tk <62570383+tksoftw@users.noreply.github.com>
2026-02-18 13:33:51 -0500

    update

--- vmexplore/lab.c
+++ vmexplore/lab.c
@@ -4,18 +4,49 @@
 #include <stdlib.h>     // for atoi (and malloc() which you'll likely use)
 #include <sys/mman.h>   // for mmap() which you'll likely use
 #include <stdalign.h>
+#include <string.h>
 
-
+alignas(4096) volatile char global_array[4096 * 32];
 
 void labStuff(int which) {
     if (which == 0) {
         /* do nothing */
     } else if (which == 1) {
-
+        
+        memset(global_array, 0, 4096); // writes in just 1 page => 1 page fault
+        
+        int flags = MAP_PRIVATE | MAP_ANON | MAP_FIXED;
+        char* mapped_addr = mmap(
+            global_array,           // addr
+            4096,                   // size
+            PROT_READ | PROT_WRITE, // prot 
+            flags,                  // flags
+            -1,                     // fd (none)
+            0                       // offset
+        ); // remap first 4096 bytes (1 page) to a fresh AoD page
+        
+        memset(global_array, 0, 8192); // write crosses 2 pages => 2 page fault
+        
+        memset(global_array, 1, 8192); // should incur no page fault
+    
     } else if (which == 2) {
+        long one_mib = 1024*1024; // (guarenteed to be aligned bc a multiple of pg size)
+        int flags = MAP_PRIVATE | MAP_ANON;
+        char* mapped_addr = mmap(
+            0,                      // addr (automatic)
+            one_mib,                // size
+            PROT_READ | PROT_WRITE, // prot 
+            flags,                  // flags
+            -1,                     // fd (none)
+            0                       // offset
+        ); // remap 1MiB (256 pages) to fresh AoD pages
+        mapped_addr[0]; // fault
+        mapped_addr[2]; // no fault
+        mapped_addr[4097]; // fault
+        mapped_addr[6767]; // no fault
 
     } else if (which == 3) {
-
+        
     } else if (which == 4) {
 
     }
